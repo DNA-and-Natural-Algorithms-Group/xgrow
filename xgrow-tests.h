42654a315d31b795a6203d134d939a3d8191255f
rebecka <unknown>
2005-07-30 21:16:23 +0000

    merging testing changes into the main branch

---
+++ xgrow-tests.h
@@ -0,0 +1,17 @@
+
+
+#ifndef __XGROW_TESTS_H__
+#define __XGROW_TESTS_H__
+
+#include "grow.h"
+
+typedef unsigned char ** Assembly;
+
+void run_xgrow_tests (tube *tp,double Gmc, double Gse, int seed_i, int seed_j, int seed_n, int size);
+int assembly_is_a_duplicate (void *assemblies, 
+			     Assembly a, int size);
+void add_assembly_to_seen (tube *tp);
+
+void update_state_on_indicator(flake *fp, Assembly a, int size);
+void update_state_off_indicator(flake *fp);
+#endif
