f4dad460cfd72f94456fa8560ae4afac613c7c9b
Constantine Evans <cevans@evanslabs.org>
2010-08-16 13:46:19 -0700

    Changes from cge-experimental: add min_strength option and incorrect-attachment checking that takes into account glues; add make clean; fix pointer problem in print_tree (now compiles with no warnings).

--- grow.h
+++ grow.h
@@ -89,13 +89,17 @@ double exp(); double log();
 
 #define Mism(fp,i,j,n) (                                                   \
  ((fp->tube->tileb)[n][1] != (fp->tube->tileb)[fp->Cell(i,(j)+1)][3] &&    \
-  (fp->tube->tileb)[n][1]*(fp->tube->tileb)[fp->Cell(i,(j)+1)][3] > 0) +   \
+  (fp->tube->tileb)[n][1]*(fp->tube->tileb)[fp->Cell(i,(j)+1)][3] > 0 &&   \
+  (fp->tube->glue)[(fp->tube->tileb)[n][1]][(fp->tube->tileb)[fp->Cell(i,(j)+1)][3]] < min_strength) +   \
  ((fp->tube->tileb)[n][3] != (fp->tube->tileb)[fp->Cell(i,(j)-1)][1] &&    \
-  (fp->tube->tileb)[n][3]*(fp->tube->tileb)[fp->Cell(i,(j)-1)][1] > 0) +   \
+  (fp->tube->tileb)[n][3]*(fp->tube->tileb)[fp->Cell(i,(j)-1)][1] > 0 &&   \
+  (fp->tube->glue)[(fp->tube->tileb)[n][3]][(fp->tube->tileb)[fp->Cell(i,(j)-1)][1]] < min_strength) +   \
  ((fp->tube->tileb)[n][2] != (fp->tube->tileb)[fp->Cell((i)+1,j)][0] &&    \
-  (fp->tube->tileb)[n][2]*(fp->tube->tileb)[fp->Cell((i)+1,j)][0] > 0) +   \
+  (fp->tube->tileb)[n][2]*(fp->tube->tileb)[fp->Cell((i)+1,j)][0] > 0 &&   \
+  (fp->tube->glue)[(fp->tube->tileb)[n][2]][(fp->tube->tileb)[fp->Cell((i)+1,j)][0]] < min_strength) +   \
  ((fp->tube->tileb)[n][0] != (fp->tube->tileb)[fp->Cell((i)-1,j)][2] &&    \
-  (fp->tube->tileb)[n][0]*(fp->tube->tileb)[fp->Cell((i)-1,j)][2] > 0) )
+  (fp->tube->tileb)[n][0]*(fp->tube->tileb)[fp->Cell((i)-1,j)][2] > 0 && \
+  (fp->tube->glue)[(fp->tube->tileb)[n][0]][(fp->tube->tileb)[fp->Cell((i)-1,j)][2]] < min_strength) )   
 
 
 
@@ -255,6 +259,7 @@ extern double blast_rate_alpha;
 extern double blast_rate_beta;
 extern double blast_rate_gamma;
 extern double blast_rate;
+extern double min_strength;
 extern int *present_list;
 extern int present_list_len;
 extern int untiltiles,untiltilescount;
