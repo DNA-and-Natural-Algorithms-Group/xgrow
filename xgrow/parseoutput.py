f4b5ae03d90af26cb1affd4afc4c8b14cb344f27
Constantine Evans <cevans@evanslabs.org>
2017-08-26 15:50:20 -0700

    reformat and move around files

--- xgrow/parseoutput.py
+++ xgrow/parseoutput.py
@@ -1,45 +1,55 @@
 import re
 import numpy as np
-import string
 from io import BytesIO as StringIO
 import pandas as pd
 
-def load_array(xgrowstring,onlytiles=False):
+
+def load_array(xgrowstring, onlytiles=False):
     tiles = np.genfromtxt(
-        StringIO(                                       # genfromtxt needs an io object
-            (re.sub(r'(\[|\])','',                       # remove [ and ]
-                    re.sub(r'; \.\.\.','',xgrowstring))).encode()),   # remove ; ... at line ends
-        skip_header=4,                                  # remove first lines
-        skip_footer=1,                                  # remove end junk
+        StringIO(
+            (re.sub(r'(\[|\])', '',   # remove [ and ]
+             re.sub(r'; \.\.\.', '',  # remove ; ... at line ends
+                    xgrowstring))
+             ).encode()),
+        skip_header=4,                # remove first lines
+        skip_footer=1,                # remove end junk
         dtype='uint'
         )
     if onlytiles:
         return tiles
     data = pd.Series(
         np.genfromtxt(StringIO((xgrowstring.split('\n')[2]).encode()))[1:-1],
-        index = ['gmc','gse','k','time','tiles','mismatches','events','perimeter','g','dgbonds']
-        )
+        index=['gmc', 'gse', 'k', 'time', 'tiles', 'mismatches', 'events',
+               'perimeter', 'g', 'dgbonds'])
     return {'data': data, 'tiles': tiles}
+
 
 def load_trace(s):
     data = pd.DataFrame(
-    np.genfromtxt(StringIO(s.encode())),
-        columns = ['gmc','gse','k','time','tiles','mismatches','events','perimeter','g','dgbonds'])
+        np.genfromtxt(StringIO(s.encode())),
+        columns=['gmc', 'gse', 'k', 'time', 'tiles', 'mismatches', 'events',
+                 'perimeter', 'g', 'dgbonds'])
     return data
+
 
 def load_data(s):
     data = pd.Series(
-    np.genfromtxt(StringIO(s.encode())),
-        index = ['gmc','gse','k','time','tiles','mismatches','events','perimeter','g','dgbonds'])
+        np.genfromtxt(StringIO(s.encode())),
+        index=['gmc', 'gse', 'k', 'time', 'tiles', 'mismatches', 'events',
+               'perimeter', 'g', 'dgbonds'])
     return data
+
 
 def show_array(a, ts, **kwargs):
     import matplotlib.pyplot as plt
     import matplotlib.colors as colors
     from alhambra.tiletypes import xcolors  # FIXME:  put in here!
-    mcolors = { n: tuple( z/255.0 for z in eval( x[3:] ) ) for n,x in xcolors.items() }
-    cmap = colors.ListedColormap(['black']+[mcolors[x['color']] for x in ts['tiles']])
+    mcolors = {n: tuple(z / 255.0 for z in eval(x[3:]))
+               for n, x in xcolors.items()}
+    cmap = colors.ListedColormap(['black'] + [mcolors[x['color']]
+                                              for x in ts['tiles']])
     if 'tiles' in a.keys():
-        plt.imshow(a['tiles'], cmap=cmap, vmin=0, vmax=len(ts['tiles']), **kwargs)
+        plt.imshow(
+            a['tiles'], cmap=cmap, vmin=0, vmax=len(ts['tiles']), **kwargs)
     else:
         plt.imshow(a, cmap=cmap, vmin=0, vmax=len(ts['tiles']), **kwargs)
--- xgrow/stxg.py
+++ xgrow/stxg.py
@@ -1,77 +1,99 @@
-# Structured Xgrow libary for reading, writing and converting Structured Xgrow (STXG) files.
-# If you're just manipulating stxg files, you probably don't need this (just use yaml), unless
-# you want to use the dump function, which produces somewhat cosmetically better output. The 
-# library is primarily useful for converting to and from xgrow tile files.
+# Structured Xgrow libary for reading, writing and converting Structured Xgrow
+# (STXG) files.  If you're just manipulating stxg files, you probably don't
+# need this (just use yaml), unless you want to use the dump function, which
+# produces somewhat cosmetically better output. The library is primarily useful
+# for converting to and from xgrow tile files.
 #
 # If you just want to run xgrow with stxg files, consider using xgrow-wrap.
 
 import yaml
 import re
-from io import StringIO # Fixme: stupid Python 2/3 hack.
+from io import StringIO  # Fixme: stupid Python 2/3 hack.
 import datetime
-import copy
-import logging
 
 version = 0.5
-#import warning
 
 # Option names and their argument types.
-keyvaloptions = [ ('block', int), ('size', int), ('rand', str), ('k', float), ('Gmc', float), ('Gse', float),
-                  ('Gas', float),('Gam', float),('Gae', float),('Gfc', float),('T', float),('blast_rate_alpha', float),
-                  ('blast_rate_beta', float),('blast_rate_gamma', float),('seed', str), ('update_rate', int),
-                  ('tracefile', str), ('untiltiles', str), ('tmax', float),('emax', int),('smax', int),
-                  ('smin', int), ('untiltilescount', str),('clean_cycles', int),('error_radius', float),
-                  ('datafile', str),('arrayfile', str),('exportfile', str),('importfile', str),('min_strength', float) ]
+keyvaloptions = [
+    ('block', int), ('size', int), ('rand', str), ('k', float), ('Gmc', float),
+    ('Gse', float), ('Gas', float), ('Gam', float), ('Gae', float),
+    ('Gfc', float), ('T', float), ('blast_rate_alpha', float),
+    ('blast_rate_beta', float), ('blast_rate_gamma', float), ('seed', str),
+    ('update_rate', int), ('tracefile', str), ('untiltiles', str),
+    ('tmax', float), ('emax', int), ('smax', int), ('smin', int),
+    ('untiltilescount', str), ('clean_cycles', int), ('error_radius', float),
+    ('datafile', str), ('arrayfile', str), ('exportfile', str),
+    ('importfile', str), ('min_strength', float)
+]
 keyopts = [x[0] for x in keyvaloptions]
 
 # Options that need special handling.
 specialoptions = ['stoic', 'nowindow', 'doubletile', 'vdoubletile', 'fission']
 
 # Options that take true/fale values.
-truefalseoptions = ['pause','wander','periodic']
+truefalseoptions = ['pause', 'wander', 'periodic']
 
-# Classes for controlling flow/block style of various yaml elements. These are used in dump()
-class blockseq( dict ): pass
+
+# Classes for controlling flow/block style of various yaml elements. These are
+# used in dump()
+class blockseq(dict):
+    pass
+
+
 def blockseq_rep(dumper, data):
-    return dumper.represent_mapping( u'tag:yaml.org,2002:seq', data, flow_style=False )
-class blockmap( dict ): pass
+    return dumper.represent_mapping(
+        u'tag:yaml.org,2002:seq', data, flow_style=False)
+
+
+class blockmap(dict):
+    pass
+
+
 def blockmap_rep(dumper, data):
-    return dumper.represent_mapping( u'tag:yaml.org,2002:map', data, flow_style=False )
-class flowmap( dict ): pass
+    return dumper.represent_mapping(
+        u'tag:yaml.org,2002:map', data, flow_style=False)
+
+
+class flowmap(dict):
+    pass
+
+
 def flowmap_rep(dumper, data):
-    return dumper.represent_mapping( u'tag:yaml.org,2002:map', data, flow_style=True )
+    return dumper.represent_mapping(
+        u'tag:yaml.org,2002:map', data, flow_style=True)
+
+
 yaml.add_representer(blockseq, blockseq_rep)
 yaml.add_representer(flowmap, flowmap_rep)
 yaml.add_representer(blockmap, blockmap_rep)
 
 
-def dump( tileset, *xargs, **pargs ):
+def dump(tileset, *xargs, **pargs):
+    """stxg.dump( tileset, ... ) is a wrapper for yaml.dump that slightly
+    prettifies the output taking into account what tilesets usually look
+    like. It passes all arguments onto yaml.dump.
     """
-    stxg.dump( tileset, ... ) is a wrapper for yaml.dump that slightly prettifies
-    the output taking into account what tilesets usually look like. It passes
-    all arguments onto yaml.dump.
-    """
-    
+
     # Make tile and bond sections flow-style
-    tileset['tiles'] = [ flowmap(x) for x in tileset['tiles'] ]
-    tileset['bonds'] = [ flowmap(x) for x in tileset['bonds'] ]
-    
+    tileset['tiles'] = [flowmap(x) for x in tileset['tiles']]
+    tileset['bonds'] = [flowmap(x) for x in tileset['bonds']]
+
     # If xgrowargs is there, make it block-style
-    if 'xgrowargs' in tileset.keys(): 
+    if 'xgrowargs' in tileset.keys():
         tileset['xgrowargs'] = blockmap(tileset['xgrowargs'])
-    
+
     return yaml.dump(tileset, *xargs, **pargs)
 
 
-def load( stream, *xargs, **pargs ):
+def load(stream, *xargs, **pargs):
     """
     stxg.load( stream, ... ) is a wrapper for yaml.load that currently does
     nothing else but call yaml.load
     """
-    return yaml.load( stream, *xargs, **pargs )
+    return yaml.load(stream, *xargs, **pargs)
 
 
-def to_xgrow( stxg, stream=None ):
+def to_xgrow(stxg, stream=None):
     """
     Given an stxg structure/dict, create an xgrow tile file, and return it
     as a string. Alternatively, if the stream= parameter is given a stream,
@@ -82,31 +104,33 @@ def to_xgrow( stxg, stream=None ):
         xgrowf = stream
     else:
         xgrowf = StringIO()
-    
+
     # Add some comments:
-        
-    for n,tile in enumerate(stxg['tiles']):
+
+    for n, tile in enumerate(stxg['tiles']):
         tileedges = [str(x) for x in tile['edges']]
         for e in tileedges:
             if e != '0' and e not in [x['name'] for x in stxg['bonds']]:
-                stxg['bonds'].append( { 'name': e, 'strength': 1 } )
+                stxg['bonds'].append({'name': e, 'strength': 1})
     # Start writing the tileset
     try:
         xgrowf.write("num tile types=%d\n" % len(stxg['tiles']))
         xgrowf.write("num binding types=%d\n" % len(stxg['bonds']))
     except KeyError as e:
-        raise ConvertError("Missing section {0}.".format(e.message))
-  
-    bondhasname = [ 'name' in x.keys() for x in stxg['bonds'] ]
-    
+        raise ValueError("Missing section {0}.".format(e.message))
+
+    bondhasname = ['name' in x.keys() for x in stxg['bonds']]
+
     if all(bondhasname):
-        xgrowf.write("binding type names={ %s }\n" % " ".join([x['name'] for x in stxg['bonds']]))
+        xgrowf.write("binding type names={ %s }\n" % " ".join(
+            [x['name'] for x in stxg['bonds']]))
     elif any(bondhasname):
-        raise ConvertError("Some bonds have names, but not all. All bonds must be either named or unnamed.")
-    
+        raise ValueError("Some bonds have names, but not all. " +
+                         "All bonds must be either named or unnamed.")
+
     xgrowf.write("tile edges={\n")
-    
-    for n,tile in enumerate(stxg['tiles']):
+
+    for n, tile in enumerate(stxg['tiles']):
         xgrowf.write("{ %s }" % " ".join([str(x) for x in tile['edges']]))
         if 'stoic' in tile:
             xgrowf.write("[%g]" % tile['stoic'])
@@ -116,111 +140,124 @@ def to_xgrow( stxg, stream=None ):
         if 'name' in tile:
             xgrowf.write(tile['name'])
         xgrowf.write(" (tile #%d)\n" % n)
-    
+
     xgrowf.write("}\n")
-    
-    xgrowf.write("binding strengths={ %s }\n" % " ".join([str(x['strength']) for x in stxg['bonds']]))
-    
+
+    xgrowf.write("binding strengths={ %s }\n" % " ".join(
+        [str(x['strength']) for x in stxg['bonds']]))
+
     if 'glues' in stxg:
-        for x1,x2,g in [tuple(x) for x in stxg['glues']]:
+        for x1, x2, g in [tuple(x) for x in stxg['glues']]:
             # dereference
             if type(x1) != int:
                 try:
-                    x1 = [n for n,z in enumerate(stxg['bonds']) if z['name']==x1][0]+1
+                    x1 = [n for n, z in enumerate(stxg['bonds'])
+                          if z['name'] == x1][0] + 1
                 except IndexError:
                     print(x2)
                     raise ValueError(x2)
             if type(x2) != int:
                 try:
-                    x2 = [n for n,z in enumerate(stxg['bonds']) if z['name']==x2][0]+1
+                    x2 = [n for n, z in enumerate(stxg['bonds'])
+                          if z['name'] == x2][0] + 1
                 except IndexError:
                     print(x2)
                     raise ValueError(x2)
-            xgrowf.write("g(%d,%d)=%g\n" % (x1,x2,float(g)))
-    
+            xgrowf.write("g(%d,%d)=%g\n" % (x1, x2, float(g)))
+
     if 'xgrowargs' in stxg.keys():
-        for key,val in stxg['xgrowargs'].items():
+        for key, val in stxg['xgrowargs'].items():
             if key == 'doubletiles':
-                for x1,x2 in val:
+                for x1, x2 in val:
                     # dereference
                     if type(x1) != int:
                         try:
-                            x1 = [n for n,z in enumerate(stxg['tiles']) if z['name']==x1][0]+1
+                            x1 = [n for n, z in enumerate(stxg['tiles'])
+                                  if z['name'] == x1][0] + 1
                         except IndexError:
                             print(x1)
                             raise ValueError(x1)
                     if type(x2) != int:
                         try:
-                            x2 = [n for n,z in enumerate(stxg['tiles']) if z['name']==x2][0]+1
+                            x2 = [n for n, z in enumerate(stxg['tiles'])
+                                  if z['name'] == x2][0] + 1
                         except IndexError:
                             print(x2)
-                            raise ValueError(x2)           
-                    xgrowf.write( "doubletile=%d,%d\n" % (x1,x2) )
+                            raise ValueError(x2)
+                    xgrowf.write("doubletile=%d,%d\n" % (x1, x2))
             elif key == 'vdoubletiles':
-                for x1,x2 in val:
+                for x1, x2 in val:
                     # dereference
                     if type(x1) != int:
                         try:
-                            x1 = [n for n,z in enumerate(stxg['tiles']) if z['name']==x1][0]+1
+                            x1 = [n for n, z in enumerate(stxg['tiles'])
+                                  if z['name'] == x1][0] + 1
                         except IndexError:
                             print(x1)
                             raise ValueError(x1)
                     if type(x2) != int:
                         try:
-                            x2 = [n for n,z in enumerate(stxg['tiles']) if z['name']==x2][0]+1
+                            x2 = [n for n, z in enumerate(stxg['tiles'])
+                                  if z['name'] == x2][0] + 1
                         except IndexError:
                             print(x2)
-                            raise ValueError(x2)           
-                    xgrowf.write( "vdoubletile=%d,%d\n" % (x1,x2) ) 
+                            raise ValueError(x2)
+                    xgrowf.write("vdoubletile=%d,%d\n" % (x1, x2))
             else:
                 xgrowf.write("%s=%s\n" % (key, str(val)))
-           
-    xgrowf.write("% Tileset created by stxg.py version {0} on {1}\n".format(version, str(datetime.date.today())) )
-    
+
+    xgrowf.write("% Tileset created by stxg.py version {0} on {1}\n".format(
+        version, str(datetime.date.today())))
+
     if stream:
         return None
     else:
         return xgrowf.getvalue()
-        
-        
-def from_xgrow( xgst ):
+
+
+def from_xgrow(xgst):
     """
     Given a string of an xgrow tile file, convert it to stxg.
     
     Note that the conversion may require some work afterward.
     """
-    
+
     ntre = re.compile(r"^\s*num tile types=(\d+)", re.MULTILINE)
     nbre = re.compile(r"^\s*num binding types=(\d+)", re.MULTILINE)
     bsre = re.compile(r"^\s*binding strengths\s*=\s*{([^}]+)}", re.MULTILINE)
-    tere = re.compile(r"^\s*tile edges\s*=[\s\n]*{((?:[^{}]*{[^{}]+}[^{}]*)+)}", re.MULTILINE)
+    tere = re.compile(
+        r"^\s*tile edges\s*=[\s\n]*{((?:[^{}]*{[^{}]+}[^{}]*)+)}",
+        re.MULTILINE)
     bnre = re.compile(r"^\s*binding type names\s*=\s*{([^}]+)}", re.MULTILINE)
-    glre = re.compile(r"^\s*g\(\s*(\d+)\s*,\s*(\d+)\s*\)\s*=\s*(\S+)",re.MULTILINE)
-    
-    xgs = re.sub( r"%[^\n]\n", "\n", xgst )
-    
+    glre = re.compile(r"^\s*g\(\s*(\d+)\s*,\s*(\d+)\s*\)\s*=\s*(\S+)",
+                      re.MULTILINE)
+
+    xgs = re.sub(r"%[^\n]\n", "\n", xgst)
+
     m = ntre.search(xgs)
     if not m:
-        #warning.warn("Expected number of tile types.")
+        #  warning.warn("Expected number of tile types.")
         pass
-    ntiles = int( m.group(1) )
-        
+    ntiles = int(m.group(1))
+
     m = nbre.search(xgs)
     if not m:
-        #warning.warn("Expected number of binding types.")
+        #  warning.warn("Expected number of binding types.")
         pass
-    nbind = int( m.group(1) )
-    
+    nbind = int(m.group(1))
+
     bsstring = bsre.search(xgs).group(1)
     tstring = tere.search(xgs).group(1)
     bnmatch = bnre.search(xgs)
     if bnmatch:
         bnstring = bnmatch.group(1)
         bnames = bnstring.split()
-    
+
     bstrengths = bsstring.split()
-    tilevals = re.findall(r"{\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*}(?:\[(\S+)\])?(?:\((\S+)\))?",tstring)
-    
+    tilevals = re.findall(
+        r"{\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*}(?:\[(\S+)\])?(?:\((\S+)\))?",
+        tstring)
+
     tiles = []
     for tv in tilevals:
         tile = {}
@@ -231,68 +268,67 @@ def from_xgrow( xgst ):
             except:
                 pass
             tile['edges'].append(e)
-        if tv[4]!='':
+        if tv[4] != '':
             tile['stoic'] = float(tv[4])
-        if tv[5]!='':
+        if tv[5] != '':
             tile['color'] = tv[5]
         tiles.append(tile)
-        
+
     if bnmatch:
         assert len(bnames) == len(bstrengths)
-        bonds = [ {'name': name, 'strength': float(strength)} for name, strength in zip(bnames,bstrengths) ]
+        bonds = [{'name': name,
+                  'strength': float(strength)}
+                 for name, strength in zip(bnames, bstrengths)]
     else:
-        bonds = [ {'strength': float(strength)} for strength in bstrengths ]
-    
+        bonds = [{'strength': float(strength)} for strength in bstrengths]
+
     assert len(bonds) == nbind
     assert len(tiles) == ntiles
-    
+
     ts = {'tiles': tiles, 'bonds': bonds}
-    
+
     glues = {}
     m = glre.findall(xgs)
     if m:
-        for x1,x2,s in m:
+        for x1, x2, s in m:
             if bnmatch:
-                glues[(bnames[int(x1)-1], bnames[int(x2)-1])] = float(s)
+                glues[(bnames[int(x1) - 1], bnames[int(x2) - 1])] = float(s)
             else:
-                glues[int(x1),int(x2)] = float(s)
+                glues[int(x1), int(x2)] = float(s)
         ts['glues'] = glues
-    
+
     xgrowargs = {}
-    for opt,opttype in keyvaloptions:
-        m = re.findall( r"^\s*"+opt+r"\s*=\s*(\S+)", xgs, re.MULTILINE )
-        if len(m)>0:
+    for opt, opttype in keyvaloptions:
+        m = re.findall(r"^\s*" + opt + r"\s*=\s*(\S+)", xgs, re.MULTILINE)
+        if len(m) > 0:
             xgrowargs[opt] = opttype(m[-1])
-            
-    # fission
-    m = re.findall( r"^\s*(chunk_fission|fission|no_fission)", xgs, re.MULTILINE )
-    if len(m)>0:
+
+        # fission
+    m = re.findall(r"^\s*(chunk_fission|fission|no_fission)", xgs,
+                   re.MULTILINE)
+    if len(m) > 0:
         if m[-1] == 'chunk_fission':
             xgrowargs['fission'] = 'chunk'
-            
+
         elif m[-1] == 'fission':
             xgrowargs['fission'] = 'on'
         else:
             xgrowargs['fission'] = 'off'
-    
-    # doubletile
-    m = re.findall( r"^\s*doubletile\s*=\s*(\d+)\s*,\s*(\d+)", xgs, re.MULTILINE )
-    if m:
-        xgrowargs['doubletiles'] = [ [int(x1),int(x2)] for x1,x2 in m ]
-    
-    if re.search(r"^\s*-nw", xgs, re.MULTILINE ):
-        xgrowargs['window'] = False
-        
-    ts['xgrowargs'] = xgrowargs
-    return ts  
-    
-#
 
-# Stuff originally from yamltostxg.py. This is even less polished than everything above.
+    # doubletile
+    m = re.findall(r"^\s*doubletile\s*=\s*(\d+)\s*,\s*(\d+)", xgs,
+                   re.MULTILINE)
+    if m:
+        xgrowargs['doubletiles'] = [[int(x1), int(x2)] for x1, x2 in m]
+
+    if re.search(r"^\s*-nw", xgs, re.MULTILINE):
+        xgrowargs['window'] = False
+
+    ts['xgrowargs'] = xgrowargs
+    return ts
+
 
 def revcomp(seq):
     import stickydesign as sd
-    import numpy as np
     seq = seq.lower()
-    return "".join( reversed( [sd.wc[nt] for nt in seq] ) )
-    
+    return "".join(reversed([sd.wc[nt] for nt in seq]))
