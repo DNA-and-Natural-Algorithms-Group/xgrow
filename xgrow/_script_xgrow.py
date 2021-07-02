9a39b5afb1a3d978122a01d450f9ab80f2bd19a7
Constantine Evans <const@costinet.org>
2021-07-02 04:41:02 +0100

    Use new API for Python/yaml parsing, speed up CLI startup, general updates.

--- xgrow/_script_xgrow.py
+++ xgrow/_script_xgrow.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import re
 import yaml
@@ -13,11 +12,12 @@ parsed_traditional = {
     'fission': lambda x: ('fission', 'on'),
     'chunk_fission': lambda x: ('fission', 'chunk'),
     'importfile': lambda x: ('importfile', x.group(3)
-                             if x.group(3) else 'xgrow_export_output') }
+                             if x.group(3) else 'xgrow_export_output')}
 
 parsed_new = {
-    'w': lambda x: ('window',not bool(x.group(1))),
-    'nw': lambda x: ('window',bool(x.group(1))) }
+    'w': lambda x: ('window', not bool(x.group(1))),
+    'nw': lambda x: ('window', bool(x.group(1)))}
+
 
 def print_help():
     print("""
@@ -45,6 +45,7 @@ documentation.  In general, any option that can be used in an stxg or Xgrow file
 be used here, though there are also the shorthands -w and -nw for -window and -no-window.
     """)
 
+
 def print_options():
     print("""
 These options are currently taken from xgrow itself.  They need to be updated.
@@ -68,8 +69,8 @@ These options are currently taken from xgrow itself.  They need to be updated.
   stoic=n[s]            set stoichiometry of tile n to relative value s
   anneal=g,t            anneal Gse from g to given Gse with time constant t
   linan=h,s,C0,Cfin,dt  do a linear anneal, where delta G at each temperature is
-                        governed by (Delta) h and (Delta) s in kcals per mol.  
-                        Go from C0 > Cfin changing temps 1 degree every dt secs 
+                        governed by (Delta) h and (Delta) s in kcals per mol.
+                        Go from C0 > Cfin changing temps 1 degree every dt secs
                         (in incremenets of 0.1C).  Ignores Gse value.
   seed=i,j,n            seed tile type n at position i,j
   tinybox=V             use dynamic flakes in a box of volume V (in liters).
@@ -87,7 +88,7 @@ These options are currently taken from xgrow itself.  They need to be updated.
   no_zero_bonds          the answer is no [default]
   min_strength=         set minimum bond energy below which attachments are considered incorrect
   periodic              periodic boundary conditions
-  -linear               simulate linear A B tiles, write errs > stdout 
+  -linear               simulate linear A B tiles, write errs > stdout
   -nw                   no X window (only if ?max set)
   update_rate=          update display every so-many events
   tracefile=            append datafile info (see below) EVERY so-many events
@@ -105,9 +106,9 @@ These options are currently taken from xgrow itself.  They need to be updated.
   fill_cycles=          at end, add how many layers of strongly attached tiles?
                         [default=0]
   fill_X=               for filling, minimal ratio of off-rate to on-rate [default=1.0]
-  error_radius=         when writing to file, #mismatches counts only those for which 
+  error_radius=         when writing to file, #mismatches counts only those for which
                         all surounding tiles are present (after clean/fill) [default=0]
-  repair_unique_T=      alternative clean/fill, called Rx: remove mismatches, fill in interior sites 
+  repair_unique_T=      alternative clean/fill, called Rx: remove mismatches, fill in interior sites
                         if there is a unique strength-T tile, then fill in by strongest tile
   datafile=             append Gmc, Gse, ratek, time, size, #mismatched se, events, perimeter, dG, dG_bonds for each flake
   arrayfile=            output MATLAB-format flake array information on exit (after cleaning)
@@ -118,7 +119,8 @@ These options are currently taken from xgrow itself.  They need to be updated.
   pause                 start in paused state; wait for user to request simulation to start.
   testing               run automated tests instead of a simulation.
     """)
-    
+
+
 def main():
     # Why doesn't this use argparse or click? Because, in order to maintain
     # backward compatibility, it can't... What we want is the following:
@@ -133,13 +135,13 @@ def main():
     tilepath = None
     while pos < len(raw_args):
         # First try to parse as an argument:
-        m = re.match(r'-{1,2}(no[-_])?([^=\s]+)(=)?(\S+)?',raw_args[pos])
-        if m and m.group(2) in ['h','help']:
+        m = re.match(r'-{1,2}(no[-_])?([^=\s]+)(=)?(\S+)?', raw_args[pos])
+        if m and m.group(2) in ['h', 'help']:
             print_help()
             sys.exit(0)
         if m and m.group(2) in ['help-options']:
             print_options()
-            sys.exit(0)     
+            sys.exit(0)
         if m and m.group(2) in parsed_new.keys():
             args.__setitem__(*parsed_new[m.group(2)](m))
             pos += 1
@@ -160,36 +162,36 @@ def main():
             tilepath = raw_args[pos]
             pos += 1
         else:
-            m = re.match(r'([^=\s]+)(=)?(\S+)?',raw_args[pos])
+            m = re.match(r'([^=\s]+)(=)?(\S+)?', raw_args[pos])
+            if not m:
+                print(f"Can't parse {raw_args[pos]}.")
+                sys.exit(1)
             if m.group(1) in parsed_traditional.keys():
                 args.__setitem__(*parsed_traditional[m.group(1)](m))
             elif m.group(3):
-                args[m.group(1)]=m.group(3)
+                args[m.group(1)] = m.group(3)
             else:
-                args[m.group(1)]=True
+                args[m.group(1)] = True
             pos += 1
 
     if not tilepath:
         print("No tileset file specified!\nUse -h or --help for help.")
-        sys.exit()
-            
-    fd = open(tilepath,'r')
-    for l in fd:
-        if l[0]=="%":
+        sys.exit(1)
+
+    fd = open(tilepath, 'r')
+    for line in fd:
+        if line[0] == "%":
             continue
         else:
-            if l[0:4]=='tile' or l[0:3]=='num':
-                filetype='xgrow'
+            if line[0:4] == 'tile' or line[0:3] == 'num':
+                fd.seek(0)
+                xgrow.run_old(fd.read(), args)
+                break
             else:
-                filetype='stxg'
-                from xgrow import stxg
-            break
-    fd.seek(0)
+                fd.seek(0)
+                xgrow.run(yaml.safe_load(fd), args)
+                break
 
-    if filetype=='xgrow':
-        xgrow.run_old( fd.read(), args, process_info=False )
-    else:
-        xgrow.run( yaml.safe_load(fd), args, ui=True, process_info=False )
-    
+
 if __name__ == '__main__':
     main()
